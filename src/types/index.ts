e67a256500d07ad9c70390f438acdde1f3237e92
jkarik <yuliamatsokina@gmail.com>
2026-08-29 18:13:52 +0300

    static build

--- src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export type LetterInfo = { frequency: number; maxRepeat: number };
-
-export type AlphabetMap = Record<string, LetterInfo>;
-
-export type CountsMap = Record<string, number>;
-
-export type SortedLetterMap = Array<{letter: string, angle: number}>;
